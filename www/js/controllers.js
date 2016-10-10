e7e5a585caef252c272391f767849a2600d10f4b
unknown <18210@.(none)>
2016-10-10 09:57:30 +0800

    bin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -73,7 +73,41 @@ $controllers
 $controllers
 .controller('jiaoliu', function($rootScope, $scope, $ionicModal, $timeout, $ionicLoading, $ionicPopup, $ionicSideMenuDelegate) {
 
-
+	$ionicModal.fromTemplateUrl('jiaoliu-modal.html',{
+		'scope':$scope
+	}).then(function(modal){
+		$scope.jiaoliuModal=modal;
+	});
+	
+	$scope.open_jiaoliuModal=function(){
+		$scope.jiaoliuModal.show();
+	};
+	
+	$scope.close_jiaoliuModal = function() {
+		$scope.jiaoliuModal.hide();
+	};
+	
+	$ionicModal.fromTemplateUrl('users-modal.html',{
+		'scope':$scope
+	}).then(function(modal){
+		$scope.usersModal=modal;
+	});
+	
+	$scope.open_usersModal=function(){
+		$scope.usersModal.show();
+	};
+	
+	$scope.close_usersModal = function() {
+		$scope.usersModal.hide();
+	};
+	
+	$scope.devList = [
+              { text: "吴福康", checked: false },
+              { text: "朱学文", checked: false },
+              { text: "朱建忠", checked: false }
+            ];
+	
+	
 })
 ;
 $controllers
