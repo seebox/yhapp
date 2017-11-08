9fcaebba449cdc49d108a29ac1250df30df5cbc2
shenxh <shenxh@togetherdeMacBook-Pro.local>
2017-11-08 16:26:29 +0800

    shenxh

--- www/js/controllers/MainCtrl.js
+++ www/js/controllers/MainCtrl.js
@@ -78,6 +78,63 @@ $controllers.controller('MainCtrl', function($rootScope, $scope, $http, $statePa
             url: "app/xiaoxi/CENTER"
         }];
     });
+    $scope.playlists = [{
+        name: "引航计划",
+        url: "app/jihua"
+    }, {
+        name: "航次汇报",
+        url: "app/huibao"
+    }, {
+        name: "引航计划审批",
+        url: "app/shenpi",
+        hide: $rootScope.loginBody.dept.deptParentName == "中心机关"
+    }, {
+        name: "特种夜航船审批",
+        url: "app/tezhong",
+        hide: $rootScope.tezhong || $rootScope.yehang
+    }, {
+        name: "码头泊位信息",
+        url: "app/bowei"
+    }, {
+        name: "潮汐信息",
+        url: "app/chaoxi"
+    }, {
+        name: "出航津贴查询",
+        url: "app/butie"
+    }, {
+        name: "规章制度",
+        url: "app/faguilists/GZZD"
+    }, {
+        name: "安全预警",
+        url: "app/faguilists/AQYJ"
+    }, {
+        name: "航行公告信息",
+        url: "app/faguilists/HXGG"
+    }, {
+        name: "安全公告",
+        url: "app/faguilists/AQGG"
+    }, {
+        name: "交流互动",
+        url: "app/jiaoliu"
+    }, {
+        name: "休假管理",
+        url: "app/qingjia"
+    }, {
+        name: "网络学堂",
+        url: "app/faguilists/WLXT"
+    }, {
+        name: "公文在线",
+        url: "app/faguilists/GWZX"
+    }, {
+        name: "信息公告",
+        url: "app/faguilists/XXGG"
+    }, {
+        name: "明文传真",
+        url: "app/faguilists/MWCZ"
+    }, {
+        name: "消息中心",
+        url: "app/xiaoxi/CENTER"
+    }];
 
 
 })
